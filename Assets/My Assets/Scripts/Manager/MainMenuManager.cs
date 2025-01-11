200862a99d06f390df9eaa597d48ce8ce95b2bff
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2025-01-11 20:16:09 +0000

    :heavy_plus_sign: imported TMP
    :construction: basic main menu works
    
    :bookmark: build 0.1.3

---
+++ Assets/My Assets/Scripts/Manager/MainMenuManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// This video was used to make this Menu: https://youtu.be/DX7HyN7oJjE?si=a4BMsRd-Ofy-0Ooe
+
+namespace GolfBots.UI {
+public class MainMenuManager : MonoBehaviour {
+
+    /// <summary>
+    /// Starts the First Level Scene (Funghi Forest).
+    /// </summary>
+    public void Play() {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    /// <summary>
+    /// Exits the game.
+    /// </summary>
+    public void Quit() {
+        Application.Quit();
+    }
+
+}
+
+}
